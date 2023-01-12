d2c7a13398e8030d29c073ed621e956a21753a70
Oleh Mandra <mandra9012003@gmail.com>
2023-01-12 12:53:10 +0300

    Initial commit

---
+++ src/main/java/com/auction/auto_auction/TradeAutoAuctionApplication.java
@@ -0,0 +1,13 @@
+package com.auction.auto_auction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeAutoAuctionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradeAutoAuctionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/auction/auto_auction/TradeAutoAuctionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auction.auto_auction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradeAutoAuctionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
