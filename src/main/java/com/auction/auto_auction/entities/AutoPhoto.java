214a4b7b2baf922226768cf34092fb9e4dae032d
Oleh Mandra <mandra9012003@gmail.com>
2023-01-13 23:08:00 +0300

    created entity "AutoPhoto" which store photos cars

---
+++ src/main/java/com/auction/auto_auction/entities/AutoPhoto.java
@@ -0,0 +1,24 @@
+package com.auction.auto_auction.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "auto_photos")
+public class AutoPhoto {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "photo_src",nullable = false)
+    private String photoSrc;
+}
