abab8b9993c0746e0ca0368ec38d4fe468339ee5
Oleh Mandra <mandra9012003@gmail.com>
2023-01-13 14:59:06 +0300

    created entity "Customer"

---
+++ src/main/java/com/auction/auto_auction/entities/Customer.java
@@ -0,0 +1,25 @@
+package com.auction.auto_auction.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "customers")
+public class Customer {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "discount")
+    private int discount;
+
+}
