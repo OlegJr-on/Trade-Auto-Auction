2f0c5c7ac1c300d8089e7d562491c54264e4dbc4
Oleh Mandra <mandra9012003@gmail.com>
2023-01-13 20:55:55 +0300

    created RoleRepository with JPA default functionality

---
+++ src/main/java/com/auction/auto_auction/repositories/RoleRepository.java
@@ -0,0 +1,7 @@
+package com.auction.auto_auction.repositories;
+
+import com.auction.auto_auction.entities.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role,Integer> {
+}
