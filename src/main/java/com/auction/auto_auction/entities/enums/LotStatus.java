1977dbc8e8242883a2eac8339d6b902fa53816e7
Oleh Mandra <mandra9012003@gmail.com>
2023-01-13 23:38:36 +0300

    created "LotStatus" for definition state of lot

---
+++ src/main/java/com/auction/auto_auction/entities/enums/LotStatus.java
@@ -0,0 +1,14 @@
+package com.auction.auto_auction.entities.enums;
+
+public enum LotStatus {
+    NOT_ACTIVE("Not active"),
+    SOLD_OUT("Sold out"),
+    TRADING("Trading"),
+    OVERDUE("Overdue");
+
+    public final String label;
+
+    private LotStatus(String label) {
+        this.label = label;
+    }
+}
