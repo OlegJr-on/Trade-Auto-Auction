08b65c0059f841fe723dc61ddae88c7c1488928e
Oleh Mandra <mandra9012003@gmail.com>
2023-01-13 16:03:01 +0300

    created entity "BankAccount" which represents a common balance money of customer

---
+++ src/main/java/com/auction/auto_auction/entities/BankAccount.java
@@ -0,0 +1,27 @@
+package com.auction.auto_auction.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "bank_accounts")
+public class BankAccount {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "balance",nullable = false)
+    private BigDecimal balance;
+
+}
