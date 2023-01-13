f9938d0c72f0066767d313d84727d79b68b5b0dc
Oleh Mandra <mandra9012003@gmail.com>
2023-01-13 20:19:26 +0300

    added ability to execute all HTTP-methods for API without authentication

--- src/main/java/com/auction/auto_auction/security/SecurityConfig.java
+++ src/main/java/com/auction/auto_auction/security/SecurityConfig.java
@@ -1,11 +1,31 @@
 package com.auction.auto_auction.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableMethodSecurity
 public class SecurityConfig {
 
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
+        // Allow All Http GET-Methods without authenticate
+        // http.csrf().disable()
+        // .authorizeHttpRequests((authorize) -> authorize.requestMatchers(HttpMethod.GET,"/**")
+        //              .permitAll().anyRequest().authenticated())
+        // .httpBasic(Customizer.withDefaults());
+
+        http.csrf().disable()
+                .authorizeHttpRequests(authorize -> authorize.requestMatchers(request -> true)
+                        .permitAll().anyRequest().authenticated()
+                );
+
+        return http.build();
+    }
 }
