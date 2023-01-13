b31d744262d1f8393f11c9bcea9fc6f69c79cb3a
Oleh Mandra <mandra9012003@gmail.com>
2023-01-13 16:29:07 +0300

    created class for Spring Security configuration

---
+++ src/main/java/com/auction/auto_auction/security/SecurityConfig.java
@@ -0,0 +1,11 @@
+package com.auction.auto_auction.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+
+@Configuration
+@EnableMethodSecurity
+public class SecurityConfig {
+
+
+}
