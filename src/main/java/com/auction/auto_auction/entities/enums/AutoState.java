64c85620ade3c4b89a261a177df77982090ba1c9
Oleh Mandra <mandra9012003@gmail.com>
2023-01-13 22:58:21 +0300

    created "AutoState" for definition state of car

---
+++ src/main/java/com/auction/auto_auction/entities/enums/AutoState.java
@@ -0,0 +1,13 @@
+package com.auction.auto_auction.entities.enums;
+
+public enum AutoState {
+    RUN_AND_DRIVE("Run and drive"),
+    NON_OPERATIONAL("Non-Operational"),
+    USED("Used");
+
+    public final String label;
+
+    private AutoState(String label) {
+        this.label = label;
+    }
+}
