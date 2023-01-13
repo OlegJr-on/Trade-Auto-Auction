5d3d8b1051c4f9c9e5b0cec713b3fb0d128f056f
Oleh Mandra <mandra9012003@gmail.com>
2023-01-14 00:36:35 +0300

    created "SalesDepartmentRepository" with JPA default functionality

---
+++ src/main/java/com/auction/auto_auction/repositories/SalesDepartmentRepository.java
@@ -0,0 +1,7 @@
+package com.auction.auto_auction.repositories;
+
+import com.auction.auto_auction.entities.SalesDepartment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SalesDepartmentRepository extends JpaRepository<SalesDepartment,Integer> {
+}
