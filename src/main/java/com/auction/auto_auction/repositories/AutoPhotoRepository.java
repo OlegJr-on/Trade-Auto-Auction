f2699dec697393f689a5382220fce110009ea03a
Oleh Mandra <mandra9012003@gmail.com>
2023-01-13 23:16:49 +0300

    created AutoPhotoRepository with JPA default functionality

---
+++ src/main/java/com/auction/auto_auction/repositories/AutoPhotoRepository.java
@@ -0,0 +1,7 @@
+package com.auction.auto_auction.repositories;
+
+import com.auction.auto_auction.entities.AutoPhoto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AutoPhotoRepository extends JpaRepository<AutoPhoto,Integer> {
+}
