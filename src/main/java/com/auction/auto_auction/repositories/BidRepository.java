6ea9812571a676173351b312bcd8fc885cef83fa
Oleh Mandra <mandra9012003@gmail.com>
2023-01-14 16:07:37 +0300

    added method findByCustomerId() which search customer by customer id

--- src/main/java/com/auction/auto_auction/repositories/BidRepository.java
+++ src/main/java/com/auction/auto_auction/repositories/BidRepository.java
@@ -7,4 +7,5 @@ import java.util.Optional;
 
 public interface BidRepository extends JpaRepository<Bid,Integer> {
     Optional<Bid> findByLotId(int lotId);
+    Optional<Bid> findByCustomerId(int customerId);
 }
