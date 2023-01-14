4ed83ca25f66476768f43c9ba2f1eaadee5606d7
Oleh Mandra <mandra9012003@gmail.com>
2023-01-14 16:06:41 +0300

    added method findByLotId() which search bid by lot id

--- src/main/java/com/auction/auto_auction/repositories/BidRepository.java
+++ src/main/java/com/auction/auto_auction/repositories/BidRepository.java
@@ -3,5 +3,8 @@ package com.auction.auto_auction.repositories;
 import com.auction.auto_auction.entities.Bid;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface BidRepository extends JpaRepository<Bid,Integer> {
+    Optional<Bid> findByLotId(int lotId);
 }
