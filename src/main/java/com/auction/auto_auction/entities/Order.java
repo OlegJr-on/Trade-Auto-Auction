ecf027b11b9515f3b7c919a94bfee5a48bb55b41
Oleh Mandra <mandra9012003@gmail.com>
2023-01-15 00:13:52 +0300

    created entity "Order" which will store succeed bid

---
+++ src/main/java/com/auction/auto_auction/entities/Order.java
@@ -0,0 +1,24 @@
+package com.auction.auto_auction.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "orders")
+public class Order {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+
+}
