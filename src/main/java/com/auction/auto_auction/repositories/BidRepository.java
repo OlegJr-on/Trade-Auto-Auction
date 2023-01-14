99b2e6e75a788eae6ed1d5baf50a4b438c3b1cf2
Oleh Mandra <mandra9012003@gmail.com>
2023-01-14 20:22:36 +0300

    fix methods, changed return type into List<>

--- src/main/java/com/auction/auto_auction/repositories/BidRepository.java
+++ src/main/java/com/auction/auto_auction/repositories/BidRepository.java
@@ -1,11 +1,13 @@
 package com.auction.auto_auction.repositories;
 
 import com.auction.auto_auction.entities.Bid;
+import com.auction.auto_auction.entities.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface BidRepository extends JpaRepository<Bid,Integer> {
-    Optional<Bid> findByLotId(int lotId);
-    Optional<Bid> findByCustomerId(int customerId);
+    Optional<List<Bid>> findByLotId(int lotId);
+    Optional<List<Customer>> findByCustomerId(int customerId);
 }
