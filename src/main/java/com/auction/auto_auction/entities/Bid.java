c19b3e828d9f670b6ea0498372422839eb9a56c6
Oleh Mandra <mandra9012003@gmail.com>
2023-01-14 15:37:27 +0300

    created entity "Bid" which will execute bet logic

---
+++ src/main/java/com/auction/auto_auction/entities/Bid.java
@@ -0,0 +1,33 @@
+package com.auction.auto_auction.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "bids")
+public class Bid {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "operation_date",nullable = false)
+    private Date operationDate;
+
+    @Column(name = "is_active",nullable = false)
+    private boolean isActive;
+
+    @Column(name = "bet",nullable = false)
+    private BigDecimal bet;
+}
