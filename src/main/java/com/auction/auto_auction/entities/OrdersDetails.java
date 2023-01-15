8c1cb4913c439cc74c7db2beed20a290f735790d
Oleh Mandra <mandra9012003@gmail.com>
2023-01-15 11:56:45 +0300

    created entity "OrdersDetails" which represents final receipt

---
+++ src/main/java/com/auction/auto_auction/entities/OrdersDetails.java
@@ -0,0 +1,30 @@
+package com.auction.auto_auction.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "order_details")
+public class OrdersDetails {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "auction_rate",nullable = false)
+    private int auctionRate;
+
+    @Column(name = "total_price",nullable = false)
+    private BigDecimal totalPrice;
+}
