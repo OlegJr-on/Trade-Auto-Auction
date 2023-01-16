4f3ee6b3f415d145beacdc3858cee5e2b04bfb16
Oleh Mandra <mandra9012003@gmail.com>
2023-01-16 21:54:32 +0300

    refactoring (added space between methods)

--- src/main/java/com/auction/auto_auction/repositories/uow/UnitOfWork.java
+++ src/main/java/com/auction/auto_auction/repositories/uow/UnitOfWork.java
@@ -6,14 +6,25 @@ import com.auction.auto_auction.repositories.*;
 public interface UnitOfWork {
 
     AutoPhotoRepository getAutoPhotoRepository();
+
     BankAccountRepository getBankAccountRepository();
+
     BidRepository getBidRepository();
+
     CarRepository getCarRepository();
+
     CustomerRepository getCustomerRepository();
+
     LotRepository getLotRepository();
+
     OrderRepository getOrderRepository();
+
     OrdersDetailsRepository getOrdersDetailsRepository();
+
     RoleRepository getRoleRepository();
+
     SalesDepartmentRepository getSalesDepartmentRepository();
+
     UserRepository getUserRepository();
+
 }
