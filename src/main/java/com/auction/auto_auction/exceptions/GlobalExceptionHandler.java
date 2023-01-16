798c4bcd2371cde5c414e86a122cb45ca0a3eacb
Oleh Mandra <mandra9012003@gmail.com>
2023-01-16 17:20:28 +0300

    added exception handler for "ResourceNotFoundException"

--- src/main/java/com/auction/auto_auction/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/auction/auto_auction/exceptions/GlobalExceptionHandler.java
@@ -1,10 +1,27 @@
 package com.auction.auto_auction.exceptions;
 
+import com.auction.auto_auction.DTOs.ErrorDetails;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.Date;
 
 @ControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorDetails> handleResourceNotFoundException(ResourceNotFoundException exception,
+                                                                        WebRequest webRequest){
+        ErrorDetails errorDetails = new ErrorDetails(
+                new Date(),
+                exception.getMessage(),
+                webRequest.getDescription(false),
+                HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(errorDetails,HttpStatus.NOT_FOUND);
+    }
 
 }
