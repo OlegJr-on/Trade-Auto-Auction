43c238f6e9094b19e5f68d897774c1d93a060a54
Oleh Mandra <mandra9012003@gmail.com>
2023-01-16 17:15:55 +0300

    created exception handler for a correct response view

---
+++ src/main/java/com/auction/auto_auction/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,10 @@
+package com.auction.auto_auction.exceptions;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+
+}
