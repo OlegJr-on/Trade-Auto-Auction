9805b262fdf6b4509cfd41fc3ef0d349e09161e7
Oleh Mandra <mandra9012003@gmail.com>
2023-01-17 21:12:39 +0300

    refactor: rename folder; add annotation for support constructor without parameters

--- src/main/java/com/auction/auto_auction/exception/ResourceNotFoundException.java
+++ src/main/java/com/auction/auto_auction/exception/ResourceNotFoundException.java
@@ -1,26 +1,16 @@
-package com.auction.auto_auction.exceptions;
+package com.auction.auto_auction.exception;
 
-import lombok.Getter;
+import lombok.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Getter
+@NoArgsConstructor
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException{
-
     private String sourceName;
-
     private String fieldName;
-
     private String fieldValue;
-
-
-    public ResourceNotFoundException(String message, String sourceName, String fieldName, String fieldValue) {
-        super(message);
-        this.sourceName = sourceName;
-        this.fieldName = fieldName;
-        this.fieldValue = fieldValue;
-    }
 
     public ResourceNotFoundException(String sourceName, String fieldName, String fieldValue) {
         super(String.format("In %s not found %s with value: %s",sourceName,fieldName,fieldValue));
