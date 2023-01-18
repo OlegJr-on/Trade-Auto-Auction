f1ca79f688652ced9881cc4f0a3e9702f69908d3
Oleh Mandra <mandra9012003@gmail.com>
2023-01-18 18:13:57 +0300

    feat: add methods which return user by email, by first and last name

--- src/main/java/com/auction/auto_auction/repository/UserRepository.java
+++ src/main/java/com/auction/auto_auction/repository/UserRepository.java
@@ -3,5 +3,14 @@ package com.auction.auto_auction.repository;
 import com.auction.auto_auction.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User,Integer> {
+
+    Optional<List<User>> findByFirstName(String firstName);
+
+    Optional<User> findByFirstNameAndLastName(String firstName,String lastName);
+
+    Optional<User> findByEmail(String email);
 }
