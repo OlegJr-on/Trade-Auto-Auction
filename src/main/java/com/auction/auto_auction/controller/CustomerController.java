c1e41cf76e0541d66b41b1c3d4feb0fc2de8ba37
Oleh Mandra <mandra9012003@gmail.com>
2023-01-19 23:04:47 +0300

    feat: ability to get customer by his email, first and last names

--- src/main/java/com/auction/auto_auction/controller/CustomerController.java
+++ src/main/java/com/auction/auto_auction/controller/CustomerController.java
@@ -2,12 +2,14 @@ package com.auction.auto_auction.controller;
 
 import com.auction.auto_auction.dto.CustomerDTO;
 import com.auction.auto_auction.service.CustomerService;
+import com.auction.auto_auction.utils.ApplicationConstants;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Collections;
 import java.util.List;
 
 
@@ -22,7 +24,7 @@ public class CustomerController {
     }
 
     @GetMapping
-    public ResponseEntity<List<CustomerDTO>> getAllCustomers(){
+    public ResponseEntity<List<CustomerDTO>> getAllCustomers() {
 
         List<CustomerDTO> customers = this.customerService.findAll();
 
@@ -30,11 +32,45 @@ public class CustomerController {
     }
 
     @GetMapping(path = "/{id}")
-    public ResponseEntity<CustomerDTO> getCustomerById(@PathVariable("id") int customerId){
+    public ResponseEntity<CustomerDTO> getCustomerById(@PathVariable("id") int customerId) {
 
         CustomerDTO customer = this.customerService.findById(customerId);
 
         return ResponseEntity.ok(customer);
+    }
+
+    @GetMapping(path = "/by")
+    public ResponseEntity<List<CustomerDTO>> getCustomerByProperties(
+            @RequestParam(value = "firstName",defaultValue = ApplicationConstants.EMPTY_STRING) String firstName,
+            @RequestParam(value = "lastName",defaultValue = ApplicationConstants.EMPTY_STRING) String lastName,
+            @RequestParam(value = "email",defaultValue = ApplicationConstants.EMPTY_STRING) String email
+    ){
+
+        List<CustomerDTO> customers = null;
+
+        // find by only firstName
+        if (!firstName.isEmpty() && lastName.isEmpty()){
+            customers = this.customerService.findByFirstName(firstName);
+        }
+
+        // find by firstName and lastName
+        if (!firstName.isEmpty() && !lastName.isEmpty()){
+            customers = Collections.singletonList(
+                    this.customerService.findByFirstNameAndLastName(firstName,lastName));
+        }
+
+        // find by only email, because email is unique for any customers
+        if (!email.isEmpty()){
+            customers = Collections.singletonList(
+                    this.customerService.findByEmail(email));
+        }
+
+        // if all properties entered wrong - throw exception
+        if (customers == null){
+            throw new NullPointerException("Entered data is incorrect");
+        }
+
+        return ResponseEntity.ok(customers);
     }
 
     @PostMapping
