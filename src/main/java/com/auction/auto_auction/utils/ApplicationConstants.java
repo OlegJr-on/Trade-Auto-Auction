470630dfb8a8579703c07ac8e897216140deb5ed
Oleh Mandra <mandra9012003@gmail.com>
2023-01-19 18:44:56 +0300

    feat: add constant who store role id by default(its UserRole), which will set in new customer

--- src/main/java/com/auction/auto_auction/utils/ApplicationConstants.java
+++ src/main/java/com/auction/auto_auction/utils/ApplicationConstants.java
@@ -1,5 +1,8 @@
 package com.auction.auto_auction.utils;
 
+import com.auction.auto_auction.entity.Role;
+
 public class ApplicationConstants {
     public static final String EMPTY_STRING = "";
+    public static final int DEFAULT_ROLE_ID = 2;
 }
