6ac5e57ff0abdb5a9607d81c81e8ee47e202c671
Oleh Mandra <mandra9012003@gmail.com>
2023-01-19 22:13:57 +0300

    feat: add method which return all customers; received data will transfer into json

--- src/main/java/com/auction/auto_auction/controller/CustomerController.java
+++ src/main/java/com/auction/auto_auction/controller/CustomerController.java
@@ -2,7 +2,10 @@ package com.auction.auto_auction.controller;
 
 import com.auction.auto_auction.dto.CustomerDTO;
 import com.auction.auto_auction.service.CustomerService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @RestController
@@ -15,5 +18,11 @@ public class CustomerController {
         this.customerService = customerService;
     }
 
+    @GetMapping(produces = "application/json")
+    public ResponseEntity<List<CustomerDTO>> getAllCustomers(){
 
+        List<CustomerDTO> customers = this.customerService.findAll();
+
+        return ResponseEntity.ok(customers);
+    }
 }
