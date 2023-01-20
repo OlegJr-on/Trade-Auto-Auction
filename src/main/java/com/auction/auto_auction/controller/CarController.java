19f5e390c33348fd538ba0e64f33add640c2e282
Oleh Mandra <mandra9012003@gmail.com>
2023-01-20 22:43:36 +0300

    feat: create controller which implements car's business logic and transfer data into away

---
+++ src/main/java/com/auction/auto_auction/controller/CarController.java
@@ -0,0 +1,26 @@
+package com.auction.auto_auction.controller;
+
+import com.auction.auto_auction.dto.CarDTO;
+import com.auction.auto_auction.dto.CustomerDTO;
+import com.auction.auto_auction.service.CarService;
+import com.auction.auto_auction.utils.ApplicationConstants;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collections;
+import java.util.List;
+
+@RestController
+@RequestMapping("api-auction/cars")
+public class CarController {
+
+    private final CarService carService;
+
+    public CarController(CarService carService) {
+        this.carService = carService;
+    }
+
+}
