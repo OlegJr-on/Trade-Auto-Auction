561c940718e21aac63ee794bdb98bd9b43984658
Oleh Mandra <mandra9012003@gmail.com>
2023-01-20 19:16:12 +0300

    feat: add method which execute query and returns car by it`s mark and model

--- src/main/java/com/auction/auto_auction/repository/CarRepository.java
+++ src/main/java/com/auction/auto_auction/repository/CarRepository.java
@@ -3,5 +3,10 @@ package com.auction.auto_auction.repository;
 import com.auction.auto_auction.entity.Car;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface CarRepository extends JpaRepository<Car,Integer> {
+
+    Optional<List<Car>> findByMarkAndModel(String mark,String model);
 }
