593b9de346c72482e2c834454e0601db05fc70e6
Oleh Mandra <mandra9012003@gmail.com>
2023-01-22 17:21:42 +0300

    feat: add builder pattern for entity

--- src/main/java/com/auction/auto_auction/entity/Lot.java
+++ src/main/java/com/auction/auto_auction/entity/Lot.java
@@ -3,10 +3,7 @@ package com.auction.auto_auction.entity;
 import com.auction.auto_auction.entity.enums.LotStatus;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
@@ -17,6 +14,7 @@ import java.util.List;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "lots")
 @JsonIgnoreProperties({"salesInfo","bids"})
