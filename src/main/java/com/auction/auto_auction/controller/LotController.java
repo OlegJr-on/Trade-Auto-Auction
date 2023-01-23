d9d44c5ca76bbd4c1f53d03ce5de4dcb50786a92
Oleh Mandra <mandra9012003@gmail.com>
2023-01-23 22:47:50 +0300

    feat: create controller which implements lot's business logic and transfer data into away

---
+++ src/main/java/com/auction/auto_auction/controller/LotController.java
@@ -0,0 +1,24 @@
+package com.auction.auto_auction.controller;
+
+import com.auction.auto_auction.dto.LotDTO;
+import com.auction.auto_auction.service.LotService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@RestController
+@RequestMapping("api-auction/lots")
+public class LotController {
+
+    private final LotService lotService;
+
+    public LotController(LotService lotService) {
+        this.lotService = lotService;
+    }
+
+}
