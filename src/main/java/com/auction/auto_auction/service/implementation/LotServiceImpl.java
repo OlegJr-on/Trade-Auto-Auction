00e3d6315e96f004a695a8d4676ff7a1d92e5873
Oleh Mandra <mandra9012003@gmail.com>
2023-01-23 19:28:31 +0300

    feat: implements interface which has basic business exercises; override methods of interface

--- src/main/java/com/auction/auto_auction/service/implementation/LotServiceImpl.java
+++ src/main/java/com/auction/auto_auction/service/implementation/LotServiceImpl.java
@@ -13,11 +13,61 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
-public class LotServiceImpl {
+public class LotServiceImpl implements LotService{
     private final UnitOfWork unitOfWork;
 
     public LotServiceImpl(UnitOfWork unitOfWork) {
         this.unitOfWork = unitOfWork;
     }
 
+    @Override
+    public List<LotDTO> getAll() {
+        return null;
+    }
+
+    @Override
+    public LotDTO getById(int lotId) {
+        return null;
+    }
+
+    @Override
+    public LotDTO getByCarId(int carId) {
+        return null;
+    }
+
+    @Override
+    public List<LotDTO> getByDatePeriod(LocalDateTime start, LocalDateTime end) {
+        return null;
+    }
+
+    @Override
+    public List<LotDTO> getByDateBefore(LocalDateTime date) {
+        return null;
+    }
+
+    @Override
+    public List<LotDTO> getByDateAfter(LocalDateTime date) {
+        return null;
+    }
+
+
+    @Override
+    public void create(LotDTO carDTO) {
+
+    }
+
+    @Override
+    public void createByExistCarId(int carId, LotDTO lotDTO) {
+
+    }
+
+    @Override
+    public void update(int lotId, LotDTO carDTO) {
+
+    }
+
+    @Override
+    public void deleteById(int lotId) {
+
+    }
 }
