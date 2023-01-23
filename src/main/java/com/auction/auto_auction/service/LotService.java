91877981dbc7cf047335aea870cff250f96ec2c2
Oleh Mandra <mandra9012003@gmail.com>
2023-01-23 19:25:13 +0300

    feat: add standard crud`s methods; besides add method which return "lots" in concrete period

---
+++ src/main/java/com/auction/auto_auction/service/LotService.java
@@ -0,0 +1,30 @@
+package com.auction.auto_auction.service;
+
+import com.auction.auto_auction.dto.LotDTO;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface LotService {
+
+    List<LotDTO> getAll();
+
+    LotDTO getById(int lotId);
+
+    LotDTO getByCarId(int carId);
+
+    List<LotDTO> getByDatePeriod(LocalDateTime start, LocalDateTime end);
+
+    List<LotDTO> getByDateBefore(LocalDateTime date);
+
+    List<LotDTO> getByDateAfter(LocalDateTime date);
+
+    void create(LotDTO lotDTO);
+
+    void createByExistCarId(int carId, LotDTO lotDTO);
+
+    void update(int lotId, LotDTO lotDTO);
+
+    void deleteById(int lotId);
+}
