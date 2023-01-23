f8c6b7f63323535738781ebcb13b7051dbc606af
Oleh Mandra <mandra9012003@gmail.com>
2023-01-23 19:23:31 +0300

    feat: add methods which execute query that return "lots" in concrete period

--- src/main/java/com/auction/auto_auction/repository/LotRepository.java
+++ src/main/java/com/auction/auto_auction/repository/LotRepository.java
@@ -3,5 +3,16 @@ package com.auction.auto_auction.repository;
 import com.auction.auto_auction.entity.Lot;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
 public interface LotRepository extends JpaRepository<Lot,Integer> {
+
+    Optional<List<Lot>> findByStartTradingBetween(LocalDateTime start, LocalDateTime end);
+
+    Optional<List<Lot>> findByStartTradingBefore(LocalDateTime date);
+
+    Optional<List<Lot>> findByStartTradingAfter(LocalDateTime date);
 }
+
