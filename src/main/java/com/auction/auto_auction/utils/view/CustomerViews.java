61478b6c7d2e1e4c6586ba52f0c8d898dea64d97
Oleh Mandra <mandra9012003@gmail.com>
2023-01-24 23:49:24 +0300

    feat: create customer view which set visibility by several levels

---
+++ src/main/java/com/auction/auto_auction/utils/view/CustomerViews.java
@@ -0,0 +1,10 @@
+package com.auction.auto_auction.utils.view;
+
+public class CustomerViews implements Views{
+
+    public interface UserDetails extends Public { }
+
+    public interface PasswordDetails extends UserDetails { }
+
+    public interface CustomerDetails extends Public { }
+}
