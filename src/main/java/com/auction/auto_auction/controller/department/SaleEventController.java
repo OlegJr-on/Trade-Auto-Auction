04abf6a2275062bf53444dc9def915119ea7b0af
Oleh Mandra <mandra9012003@gmail.com>
2023-01-24 22:03:09 +0300

    feat: create controller which implements sales events business logic and transfer data into away

---
+++ src/main/java/com/auction/auto_auction/controller/department/SaleEventController.java
@@ -0,0 +1,29 @@
+package com.auction.auto_auction.controller.department;
+
+import com.auction.auto_auction.dto.LotDTO;
+import com.auction.auto_auction.dto.SalesDepartmentDTO;
+import com.auction.auto_auction.service.SalesDepartmentService;
+import com.auction.auto_auction.utils.ApplicationConstants;
+import com.auction.auto_auction.utils.view.SalesViews;
+import com.auction.auto_auction.utils.view.Views;
+import com.fasterxml.jackson.annotation.JsonView;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@RestController
+@RequestMapping("api-auction/department/sales")
+public class SaleEventController {
+
+    private final SalesDepartmentService salesService;
+
+    public SaleEventController(SalesDepartmentService salesService) {
+        this.salesService = salesService;
+    }
+
+}
