e272e0dee57b92285ff0264b3b9a752b7d921890
Oleh Mandra <mandra9012003@gmail.com>
2023-01-24 17:13:29 +0300

    feat: add methods which execute query that return sales events in concrete period

--- src/main/java/com/auction/auto_auction/repository/SalesDepartmentRepository.java
+++ src/main/java/com/auction/auto_auction/repository/SalesDepartmentRepository.java
@@ -3,5 +3,15 @@ package com.auction.auto_auction.repository;
 import com.auction.auto_auction.entity.SalesDepartment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
 public interface SalesDepartmentRepository extends JpaRepository<SalesDepartment,Integer> {
+
+    Optional<List<SalesDepartment>> findBySalesDateBetween(LocalDateTime start, LocalDateTime end);
+
+    Optional<List<SalesDepartment>> findBySalesDateBefore(LocalDateTime date);
+
+    Optional<List<SalesDepartment>> findBySalesDateAfter(LocalDateTime date);
 }
