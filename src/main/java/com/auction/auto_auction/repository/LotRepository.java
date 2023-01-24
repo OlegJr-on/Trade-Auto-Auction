af255674b32f86111bf61f79d57d2e1eb2423fd4
Oleh Mandra <mandra9012003@gmail.com>
2023-01-24 19:35:02 +0300

    feat: add method which return many lots by their ids

--- src/main/java/com/auction/auto_auction/repository/LotRepository.java
+++ src/main/java/com/auction/auto_auction/repository/LotRepository.java
@@ -4,10 +4,13 @@ import com.auction.auto_auction.entity.Lot;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDateTime;
+import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
 public interface LotRepository extends JpaRepository<Lot,Integer> {
+
+    Optional<List<Lot>> findByIdIn(Collection<Integer> ids);
 
     Optional<List<Lot>> findByStartTradingBetween(LocalDateTime start, LocalDateTime end);
 
