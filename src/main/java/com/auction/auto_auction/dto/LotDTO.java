e5cbc60627d434d7f9c618bc0209abf890a1abaa
Oleh Mandra <mandra9012003@gmail.com>
2023-01-24 17:55:31 +0300

    fix: set default visibility on properties

--- src/main/java/com/auction/auto_auction/dto/LotDTO.java
+++ src/main/java/com/auction/auto_auction/dto/LotDTO.java
@@ -1,6 +1,8 @@
 package com.auction.auto_auction.dto;
 
+import com.auction.auto_auction.utils.view.Views;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonView;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.NotNull;
@@ -23,21 +25,26 @@ public class LotDTO {
 
     private int id;
 
+    @JsonView(Views.Public.class)
     @NotNull(message = "State of lot cannot be null")
     private String lotStatus;
 
+    @JsonView(Views.Public.class)
     @NotNull(message = "Launch price of lot cannot be null")
     @Positive(message = "Launch price of lot cannot be less than 0$")
     private BigDecimal launchPrice;
 
+    @JsonView(Views.Public.class)
     @NotNull(message = "Minimum rate of lot cannot be null")
     @Positive(message = "Minimum rate of lot cannot be less than 0$")
     private BigDecimal minRate;
 
+    @JsonView(Views.Public.class)
     @NotNull(message = "Start trading of lot cannot be null")
     @Future(message = "Start trading of lot should be later than just now")
     private LocalDateTime startTrading;
 
+    @JsonView(Views.Public.class)
     @NotNull(message = "The end trading of lot cannot be null")
     @Future(message = "The end trading of lot should be later than just now")
     private LocalDateTime endTrading;
