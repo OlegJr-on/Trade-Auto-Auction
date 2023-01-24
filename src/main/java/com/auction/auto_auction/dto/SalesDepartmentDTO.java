6a0fcecf7857bcdfe636fd08ec6b04d8c770b447
Oleh Mandra <mandra9012003@gmail.com>
2023-01-24 15:44:51 +0300

    feat: create dto for department of sales; add validation for properties; attach special annotation to view concrete properties

---
+++ src/main/java/com/auction/auto_auction/dto/SalesDepartmentDTO.java
@@ -0,0 +1,44 @@
+package com.auction.auto_auction.dto;
+
+import com.auction.auto_auction.utils.view.SalesViews;
+import com.fasterxml.jackson.annotation.JsonView;
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class SalesDepartmentDTO {
+
+    @JsonView(SalesViews.Public.class)
+    private int id;
+
+    @JsonView(SalesViews.Public.class)
+    @NotNull(message = "Name of sales cannot be null")
+    @NotBlank(message = "Name of sales cannot be empty")
+    private String salesName;
+
+    @JsonView(SalesViews.Public.class)
+    @NotNull(message = "The location of sales is required")
+    @NotBlank(message = "The location of sales cannot be empty")
+    private String location;
+
+    @JsonView(SalesViews.Public.class)
+    @NotNull(message = "Date of sales cannot be null")
+    @Future(message = "Date of sales should be later than just now")
+    private LocalDateTime salesDate;
+
+    @JsonView(SalesViews.WithTimeLeft.class)
+    private String timeLeft;
+
+    private List<LotDTO> lots;
+}
