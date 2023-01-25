400df2d7e3148ea407054324bf39cf53d35e18b9
Oleh Mandra <mandra9012003@gmail.com>
2023-01-25 23:51:24 +0300

    fix: set default visibility by bidView

--- src/main/java/com/auction/auto_auction/dto/CarDTO.java
+++ src/main/java/com/auction/auto_auction/dto/CarDTO.java
@@ -1,6 +1,8 @@
 package com.auction.auto_auction.dto;
 
+import com.auction.auto_auction.utils.view.Views;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonView;
 import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -18,22 +20,27 @@ import java.util.List;
 @Builder
 public class CarDTO {
 
+    @JsonView(Views.Public.class)
     private int id;
 
+    @JsonView(Views.Public.class)
     @NotNull(message = "Mark of car must be not null")
     @Size(min = 2,max = 20,message = "Mark of car must have at least 2 and no more than 20 characters" )
     @Pattern(regexp = "^[a-zA-Z]*$",message = "Mark of car must have only letters")
     private String mark;
 
+    @JsonView(Views.Public.class)
     @NotNull(message = "Model of car must be not null")
     @Size(min = 2,max = 30,message = "Model of car must have at least 2 and no more than 30 characters" )
     @Pattern(regexp = "^[A-Za-z0-9]*$",message = "Model of car can have only letters and numbers")
     private String model;
 
+    @JsonView(Views.Public.class)
     @NotNull(message = "First registry date of car cannot be null")
     @Past(message = "First registry date of car should be earlier than nowadays")
     private LocalDate registryDate;
 
+    @JsonView(Views.Public.class)
     @NotNull(message = "Run of car cannot be null")
     @PositiveOrZero(message = "Run of car cannot be less or equal than 0")
     private int run;
