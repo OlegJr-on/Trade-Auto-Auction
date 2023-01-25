65b998ebfc504592372eda2aad4e15d2f3d5dba0
Oleh Mandra <mandra9012003@gmail.com>
2023-01-25 23:10:55 +0300

    feat: add methods which return bids by concrete time period

--- src/main/java/com/auction/auto_auction/repository/BidRepository.java
+++ src/main/java/com/auction/auto_auction/repository/BidRepository.java
@@ -1,9 +1,9 @@
 package com.auction.auto_auction.repository;
 
 import com.auction.auto_auction.entity.Bid;
-import com.auction.auto_auction.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -12,4 +12,10 @@ public interface BidRepository extends JpaRepository<Bid,Integer> {
     Optional<List<Bid>> findByLotId(int lotId);
 
     Optional<List<Bid>> findByCustomerId(int customerId);
+
+    Optional<List<Bid>> findByOperationDateBetween(LocalDateTime start, LocalDateTime end);
+
+    Optional<List<Bid>> findByOperationDateBefore(LocalDateTime date);
+
+    Optional<List<Bid>> findByOperationDateAfter(LocalDateTime date);
 }
