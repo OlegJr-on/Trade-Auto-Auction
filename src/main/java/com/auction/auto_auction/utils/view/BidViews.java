28f145cc043048cfb1bbc0eefd10288fa21a91ff
Oleh Mandra <mandra9012003@gmail.com>
2023-01-26 00:24:20 +0300

    feat: add one level for bid which set only bid details properties

--- src/main/java/com/auction/auto_auction/utils/view/BidViews.java
+++ src/main/java/com/auction/auto_auction/utils/view/BidViews.java
@@ -2,6 +2,8 @@ package com.auction.auto_auction.utils.view;
 
 public class BidViews implements Views {
 
+    public interface BidDetails { }
+
     public interface CustomerDetails extends Public { }
 
     public interface LotDetails extends Public { }
