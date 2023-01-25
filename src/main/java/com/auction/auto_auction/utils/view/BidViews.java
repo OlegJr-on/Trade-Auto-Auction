963299409a0c1fd35b77a53ac83962e7a203980d
Oleh Mandra <mandra9012003@gmail.com>
2023-01-25 23:41:50 +0300

    feat: create bid view which set visibility by several levels

---
+++ src/main/java/com/auction/auto_auction/utils/view/BidViews.java
@@ -0,0 +1,8 @@
+package com.auction.auto_auction.utils.view;
+
+public class BidViews implements Views {
+
+    public interface CustomerDetails extends Public { }
+
+    public interface LotDetails extends Public { }
+}
