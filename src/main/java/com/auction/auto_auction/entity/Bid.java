64eac7f9c95d550885480b7900fe50d53b091ece
Oleh Mandra <mandra9012003@gmail.com>
2023-01-25 23:25:44 +0300

    feat: add annotation which realize builder pattern

--- src/main/java/com/auction/auto_auction/entity/Bid.java
+++ src/main/java/com/auction/auto_auction/entity/Bid.java
@@ -2,10 +2,7 @@ package com.auction.auto_auction.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -14,6 +11,7 @@ import java.time.LocalDateTime;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "bids")
 @JsonIgnoreProperties({"order"})
