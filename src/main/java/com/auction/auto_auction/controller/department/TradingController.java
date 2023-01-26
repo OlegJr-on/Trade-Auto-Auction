4f23e9a244feb4743e33a4e1be146e343e0cf2ea
Oleh Mandra <mandra9012003@gmail.com>
2023-01-27 00:29:30 +0300

    refactor: clear needless imports

--- src/main/java/com/auction/auto_auction/controller/department/TradingController.java
+++ src/main/java/com/auction/auto_auction/controller/department/TradingController.java
@@ -1,23 +1,18 @@
 package com.auction.auto_auction.controller.department;
 
 import com.auction.auto_auction.dto.BidDTO;
-import com.auction.auto_auction.dto.LotDTO;
-import com.auction.auto_auction.dto.SalesDepartmentDTO;
 import com.auction.auto_auction.service.TradingService;
 import com.auction.auto_auction.utils.view.BidViews;
 import com.auction.auto_auction.utils.view.Views;
 import com.fasterxml.jackson.annotation.JsonView;
-import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.parameters.P;
 import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.time.temporal.Temporal;
 import java.util.List;
 
 @RestController
