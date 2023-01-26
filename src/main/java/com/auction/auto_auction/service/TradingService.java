1d2f6551e17aa14e40619a3eabe2ec3dc3120fdb
Oleh Mandra <mandra9012003@gmail.com>
2023-01-26 20:31:45 +0300

    feat: add standard crud`s methods

---
+++ src/main/java/com/auction/auto_auction/service/TradingService.java
@@ -0,0 +1,21 @@
+package com.auction.auto_auction.service;
+
+import com.auction.auto_auction.dto.BidDTO;
+import com.auction.auto_auction.dto.LotDTO;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface TradingService {
+
+    BidDTO getById(int bidId);
+
+    List<BidDTO> getAll();
+
+    void makeBid(int customerId, int lotId, BigDecimal bet);
+
+    void editBid(int bidId, BidDTO bidDto);
+
+    void deleteById(int bidId);
+}
