52ad95a868a67831077d7412aa27a9e3f71c33e7
Oleh Mandra <mandra9012003@gmail.com>
2023-01-27 14:45:18 +0300

    feat: add method that return customers which have any bids

--- src/main/java/com/auction/auto_auction/repository/CustomerRepository.java
+++ src/main/java/com/auction/auto_auction/repository/CustomerRepository.java
@@ -3,5 +3,10 @@ package com.auction.auto_auction.repository;
 import com.auction.auto_auction.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface CustomerRepository extends JpaRepository<Customer,Integer> {
+
+    Optional<List<Customer>> findAllByBidsNotNull();
 }
