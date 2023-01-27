199cd8a8f7c855c74b703bf7e6a075e44a225e42
Oleh Mandra <mandra9012003@gmail.com>
2023-01-27 23:11:13 +0300

    feat: create car mapper uses "MapStruct" technology

---
+++ src/main/java/com/auction/auto_auction/mapper/CarMapper.java
@@ -0,0 +1,30 @@
+package com.auction.auto_auction.mapper;
+
+import com.auction.auto_auction.dto.CarDTO;
+import com.auction.auto_auction.entity.AutoPhoto;
+import com.auction.auto_auction.entity.Car;
+import com.auction.auto_auction.entity.enums.AutoState;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+@Mapper(componentModel = "spring", imports = {AutoPhoto.class, AutoState.class})
+public interface CarMapper {
+
+    CarMapper INSTANCE = Mappers.getMapper( CarMapper.class );
+
+    @Mapping(source = "state.label", target = "autoState")
+    @Mapping(target = "lot", expression = "java(null)")
+    @Mapping(target = "photosSrc",
+             expression = "java( entity.getPhotos().stream()" +
+                                                  ".map(AutoPhoto::getPhotoSrc)" +
+                                                  ".toList())")
+    CarDTO mapToDTO(Car entity);
+
+    @Mapping(target = "state", expression = "java(AutoState.transform(dto.getAutoState()))")
+    @Mapping(target = "lot", expression = "java(null)")
+    @Mapping(target = "photos", expression = "java(dto.getPhotosSrc().stream()" +
+                                                                    ".map(AutoPhoto::new)" +
+                                                                    ".toList())")
+    Car mapToEntity(CarDTO dto);
+}
