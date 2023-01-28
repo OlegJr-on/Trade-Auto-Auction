795b4fde452f563179cd166e0c1b0bf899992178
Oleh Mandra <mandra9012003@gmail.com>
2023-01-28 19:09:37 +0300

    feat: add ability to map property "isActive" to dto property "win"

--- src/main/java/com/auction/auto_auction/mapper/BidMapper.java
+++ src/main/java/com/auction/auto_auction/mapper/BidMapper.java
@@ -3,6 +3,7 @@ package com.auction.auto_auction.mapper;
 import com.auction.auto_auction.dto.BidDTO;
 import com.auction.auto_auction.entity.Bid;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring", uses = {CustomerMapper.class,LotMapper.class})
@@ -10,5 +11,6 @@ public interface BidMapper {
 
     BidMapper INSTANCE = Mappers.getMapper(BidMapper.class);
 
+    @Mapping(source = "entity.active", target = "win")
     BidDTO mapToDTO(Bid entity);
 }
