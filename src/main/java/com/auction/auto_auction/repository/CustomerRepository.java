1fea4414927814e48ed475576579f59b051f1003
Oleh Mandra <mandra9012003@gmail.com>
2023-01-29 18:33:21 +0300

    feat: add method which return customers who most spend money on lots

--- src/main/java/com/auction/auto_auction/repository/CustomerRepository.java
+++ src/main/java/com/auction/auto_auction/repository/CustomerRepository.java
@@ -2,6 +2,7 @@ package com.auction.auto_auction.repository;
 
 import com.auction.auto_auction.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
@@ -9,4 +10,12 @@ import java.util.Optional;
 public interface CustomerRepository extends JpaRepository<Customer,Integer> {
 
     Optional<List<Customer>> findAllByBidsNotNull();
+
+    @Query("SELECT c , SUM(od.totalPrice) as spendMoney FROM Customer c " +
+            "    JOIN Bid b ON c.id = b.customer.id " +
+            "        JOIN Order o ON b.id = o.bid.id " +
+            "            JOIN OrdersDetails od ON o.id = od.order.id " +
+            "GROUP BY c.id " +
+            "ORDER BY spendMoney desc")
+    Optional<List<Customer>> getCustomersWhoMostSpend();
 }
