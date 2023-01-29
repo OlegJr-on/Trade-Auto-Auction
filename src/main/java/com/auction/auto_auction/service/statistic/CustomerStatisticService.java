f9a40ffdf198070b98fd2b3adfa2075c6cb1ae26
Oleh Mandra <mandra9012003@gmail.com>
2023-01-29 19:07:08 +0300

    feat: create abstraction of customer statistic service

---
+++ src/main/java/com/auction/auto_auction/service/statistic/CustomerStatisticService.java
@@ -0,0 +1,11 @@
+package com.auction.auto_auction.service.statistic;
+
+import com.auction.auto_auction.dto.CustomerDTO;
+import com.auction.auto_auction.dto.statistic.CustomerStatisticDTO;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface CustomerStatisticService {
+
+}
