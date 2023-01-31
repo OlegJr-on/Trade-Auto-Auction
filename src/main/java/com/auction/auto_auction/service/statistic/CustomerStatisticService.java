a2a8725a1a6ecd2659d70d182b14e06da4cad63d
Oleh Mandra <mandra9012003@gmail.com>
2023-01-31 15:18:46 +0300

    feat: add method which return customerStatisticDTO who have average indicator by growth price from launch position to ultimate(when lot bought)

--- src/main/java/com/auction/auto_auction/service/statistic/CustomerStatisticService.java
+++ src/main/java/com/auction/auto_auction/service/statistic/CustomerStatisticService.java
@@ -14,4 +14,5 @@ public interface CustomerStatisticService {
 
     List<CustomerStatisticDTO> getCustomersWhoMostSpend();
 
+    List<CustomerStatisticDTO> getCustomersByAverageGrowthIndicatorForLaunchPrice();
 }
