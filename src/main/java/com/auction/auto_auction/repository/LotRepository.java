36ae6645a3109635cd9414cde1f490470ed0704b
Oleh Mandra <mandra9012003@gmail.com>
2023-01-31 20:02:44 +0300

    feat: add method which return lots excepted entered statuses of lot

--- src/main/java/com/auction/auto_auction/repository/LotRepository.java
+++ src/main/java/com/auction/auto_auction/repository/LotRepository.java
@@ -1,12 +1,14 @@
 package com.auction.auto_auction.repository;
 
 import com.auction.auto_auction.entity.Lot;
+import com.auction.auto_auction.entity.enums.LotStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 public interface LotRepository extends JpaRepository<Lot,Integer> {
 
@@ -17,5 +19,7 @@ public interface LotRepository extends JpaRepository<Lot,Integer> {
     Optional<List<Lot>> findByStartTradingBefore(LocalDateTime date);
 
     Optional<List<Lot>> findByStartTradingAfter(LocalDateTime date);
+
+    Optional<List<Lot>> findByLotStatusNotIn(Set<LotStatus> exceptedStatuses);
 }
 
