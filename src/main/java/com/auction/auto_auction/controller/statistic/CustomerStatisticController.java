4b1fae1b2d2ff23ba099da82fe12e8c4f8d1b4a1
Oleh Mandra <mandra9012003@gmail.com>
2023-02-01 22:36:57 +0300

    fix: create controller which will be present statistical data about customers into away

---
+++ src/main/java/com/auction/auto_auction/controller/statistic/CustomerStatisticController.java
@@ -0,0 +1,21 @@
+package com.auction.auto_auction.controller.statistic;
+
+import com.auction.auto_auction.dto.statistic.CustomerStatisticDTO;
+import com.auction.auto_auction.service.statistic.CustomerStatisticService;
+import com.auction.auto_auction.utils.view.CustomerStatisticViews;
+import com.fasterxml.jackson.annotation.JsonView;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api-auction/statistic/customers")
+@AllArgsConstructor
+public class CustomerStatisticController {
+    private final CustomerStatisticService customerStatisticService;
+
+}
