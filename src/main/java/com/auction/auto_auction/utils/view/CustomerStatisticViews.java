2ffd00274f3d310ea22f4a3ad3a47b5ef43669bb
Oleh Mandra <mandra9012003@gmail.com>
2023-02-01 22:30:09 +0300

    feat: create customer view for statistic which set visibility by several levels

---
+++ src/main/java/com/auction/auto_auction/utils/view/CustomerStatisticViews.java
@@ -0,0 +1,10 @@
+package com.auction.auto_auction.utils.view;
+
+public class CustomerStatisticViews implements Views{
+
+    public interface MostActivityDetails extends Public { };
+
+    public interface MostSpendDetails extends Public { };
+
+    public interface AverageGrowthIndicatorForLaunchPrice extends Public { };
+}
