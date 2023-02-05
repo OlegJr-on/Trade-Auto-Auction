6a14b9660135413d222a6e169da850e17d123d32
Oleh Mandra <mandra9012003@gmail.com>
2023-02-05 20:49:50 +0300

    feat: add property which represents default rate of auction

--- src/main/java/com/auction/auto_auction/utils/ApplicationConstants.java
+++ src/main/java/com/auction/auto_auction/utils/ApplicationConstants.java
@@ -1,8 +1,9 @@
 package com.auction.auto_auction.utils;
 
-import com.auction.auto_auction.entity.Role;
+import java.math.BigDecimal;
 
 public class ApplicationConstants {
     public static final String EMPTY_STRING = "";
     public static final int DEFAULT_ROLE_ID = 2;
+    public static final BigDecimal DEFAULT_AUCTION_RATE = BigDecimal.valueOf(0.02); // 2%
 }
