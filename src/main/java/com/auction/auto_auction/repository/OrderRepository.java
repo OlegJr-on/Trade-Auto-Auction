8e1b6404cd76b37394726994b863785fac60ec30
Oleh Mandra <mandra9012003@gmail.com>
2023-02-05 17:58:59 +0300

    feat: add methods which return orders by customer id

--- src/main/java/com/auction/auto_auction/repository/OrderRepository.java
+++ src/main/java/com/auction/auto_auction/repository/OrderRepository.java
@@ -2,6 +2,15 @@ package com.auction.auto_auction.repository;
 
 import com.auction.auto_auction.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface OrderRepository extends JpaRepository<Order,Integer> {
+
+    @Query("SELECT o FROM Order AS o" +
+            "    JOIN Bid AS b ON b.id = o.bid.id " +
+            "WHERE b.customer.id = ?1")
+    Optional<List<Order>> findOrdersByCustomerId(int customerId);
 }
