1a229755877f16b0594549572f2c7d474784f346
Oleh Mandra <mandra9012003@gmail.com>
2023-02-05 14:43:51 +0300

    feat: create order details mapper uses "MapStruct" technology; mapper has bag(property "uses" doesn`t work)

---
+++ src/main/java/com/auction/auto_auction/mapper/OrderDetailsMapper.java
@@ -0,0 +1,19 @@
+package com.auction.auto_auction.mapper;
+
+import com.auction.auto_auction.dto.ordering.OrderDetailsDTO;
+import com.auction.auto_auction.entity.OrdersDetails;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+
+
+@Mapper(componentModel = "spring", uses = LotMapper.class)
+public interface OrderDetailsMapper {
+
+    @Mappings({
+          //@Mapping(target = "lot", expression = "java(lotMapper.mapToDTO(entity.getOrder().getBid().getLot()))"),
+            @Mapping(source = "entity.orderStatus.label", target = "orderStatus"),
+            @Mapping(target = "price", expression = "java(entity.getTotalPrice().setScale(2))")
+    })
+    OrderDetailsDTO mapToDTO(OrdersDetails entity);
+}
