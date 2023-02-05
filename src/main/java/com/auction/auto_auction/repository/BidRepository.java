872c9b63ef2dc04b2928147ee12136f3f8358766
Oleh Mandra <mandra9012003@gmail.com>
2023-02-05 20:54:33 +0300

    feat: add method which return bids whose is active and not ordered

--- src/main/java/com/auction/auto_auction/repository/BidRepository.java
+++ src/main/java/com/auction/auto_auction/repository/BidRepository.java
@@ -2,6 +2,7 @@ package com.auction.auto_auction.repository;
 
 import com.auction.auto_auction.entity.Bid;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -18,4 +19,10 @@ public interface BidRepository extends JpaRepository<Bid,Integer> {
     Optional<List<Bid>> findByOperationDateBefore(LocalDateTime date);
 
     Optional<List<Bid>> findByOperationDateAfter(LocalDateTime date);
+
+    @Query("SELECT b FROM Bid AS b " +
+            "WHERE b.id NOT IN (SELECT o.bid.id FROM Order AS o) " +
+                " AND b.isActive = true " +
+                    " AND b.customer.id = ?1")
+    Optional<List<Bid>> findBidsWhoNotOrderedByCustomerId(int customerId);
 }
