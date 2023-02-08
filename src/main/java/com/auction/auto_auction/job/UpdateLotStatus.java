ee6c96d07a6ddca5470f7625315c4826af5a9ca5
Oleh Mandra <mandra9012003@gmail.com>
2023-02-08 22:56:03 +0200

    feat: create method which update lot status by schedule

--- src/main/java/com/auction/auto_auction/job/UpdateLotStatus.java
+++ src/main/java/com/auction/auto_auction/job/UpdateLotStatus.java
@@ -19,4 +19,46 @@ import java.util.Set;
 public class UpdateLotStatus {
     private final UnitOfWork unitOfWork;
 
+    @Scheduled(fixedRate = 60000*2) // 2 min
+    @Transactional
+    public void setStatusForLots(){
+
+        List<Lot> lotsFromSource = this.unitOfWork.getLotRepository()
+                .findByLotStatusNotIn(Set.of(LotStatus.SOLD_OUT,LotStatus.OVERDUE))
+                    .orElseThrow(ResourceNotFoundException::new);
+
+        LocalDateTime timeNow = LocalDateTime.now();
+
+        for (Lot lotEntity : lotsFromSource) {
+
+            LocalDateTime startTrading = lotEntity.getStartTrading();
+            LocalDateTime endTrading = lotEntity.getEndTrading();
+            List<Bid> bidsOfGivenLot = this.unitOfWork.getBidRepository()
+                    .findByLotId(lotEntity.getId())
+                    .orElseThrow(ResourceNotFoundException::new);
+
+            // set status "Trading"
+            if (startTrading.isBefore(timeNow) && endTrading.isAfter(timeNow)) {
+                lotEntity.setLotStatus(LotStatus.TRADING);
+            }
+
+            // set status "Sold out"
+            if (endTrading.isBefore(timeNow) &&
+                    bidsOfGivenLot.stream().anyMatch(Bid::isActive)) {
+                lotEntity.setLotStatus(LotStatus.SOLD_OUT);
+            }
+
+            // set status "Overdue"
+            if (endTrading.isBefore(timeNow) && bidsOfGivenLot.isEmpty()) {
+                lotEntity.setLotStatus(LotStatus.OVERDUE);
+            }
+
+            // set status "Not active"
+            if (startTrading.isAfter(timeNow)) {
+                lotEntity.setLotStatus(LotStatus.NOT_ACTIVE);
+            }
+
+            this.unitOfWork.getLotRepository().save(lotEntity);
+        }
+    }
 }
