48251f4754558e08754011180e46e475feade2d2
Oleh Mandra <mandra9012003@gmail.com>
2023-02-08 18:53:57 +0200

    feat: add method which paying an order for customer

--- src/main/java/com/auction/auto_auction/controller/department/PaymentController.java
+++ src/main/java/com/auction/auto_auction/controller/department/PaymentController.java
@@ -2,6 +2,10 @@ package com.auction.auto_auction.controller.department;
 
 import com.auction.auto_auction.service.ReceiptService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +15,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class PaymentController {
     private final ReceiptService receiptService;
 
+    @PostMapping("/customers/{customerId}/orders/{orderId}")
+    public ResponseEntity<String> payOrder(
+            @PathVariable("customerId") int customerId,
+            @PathVariable("orderId") int orderId
+    ){
+        this.receiptService.payOrder(customerId,orderId);
+
+        return new ResponseEntity<>("Order was paid", HttpStatus.OK);
+    }
 }
