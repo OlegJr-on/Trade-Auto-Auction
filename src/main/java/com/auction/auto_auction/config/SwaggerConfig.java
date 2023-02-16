2860942cf666bcdad21c3309c7b4f09c41265162
Oleh Mandra <mandra9012003@gmail.com>
2023-02-16 22:20:29 +0200

    feat: add configuration for swagger

--- src/main/java/com/auction/auto_auction/config/SwaggerConfig.java
+++ src/main/java/com/auction/auto_auction/config/SwaggerConfig.java
@@ -1,8 +1,13 @@
 package com.auction.auto_auction.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.context.annotation.Configuration;
 
 
 @Configuration
+@OpenAPIDefinition(info = @Info(title = "Auction REST API",
+                                version = "1.0",
+                                description = "Auto Auction REST API`s documentation"))
 public class SwaggerConfig {
 }
