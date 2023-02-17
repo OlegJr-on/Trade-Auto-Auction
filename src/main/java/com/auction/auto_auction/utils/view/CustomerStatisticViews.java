2661a03873864094f1c9c58026d07e9d0262aa48
Oleh Mandra <mandra9012003@gmail.com>
2023-02-17 14:02:56 +0200

    feat: add new one level of visibility for customer statistic, that`s average indicator of the bid closeness to nominal price

--- src/main/java/com/auction/auto_auction/utils/view/CustomerStatisticViews.java
+++ src/main/java/com/auction/auto_auction/utils/view/CustomerStatisticViews.java
@@ -2,9 +2,11 @@ package com.auction.auto_auction.utils.view;
 
 public class CustomerStatisticViews implements Views{
 
-    public interface MostActivityDetails extends Public { };
+    public interface MostActivityDetails extends Public { }
 
-    public interface MostSpendDetails extends Public { };
+    public interface MostSpendDetails extends Public { }
 
-    public interface AverageGrowthIndicatorForLaunchPrice extends Public { };
+    public interface AverageGrowthIndicatorForLaunchPrice extends Public { }
+
+    public interface AverageIndicatorClosenessToNominalPrice extends Public { }
 }
