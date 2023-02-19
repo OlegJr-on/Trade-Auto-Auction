d2a213a204e5ab66ea5b5d04acb2444a1e713bbd
Oleh Mandra <mandra9012003@gmail.com>
2023-02-19 15:10:08 +0200

    feat: method which return top 10 most popular Marks

--- src/main/java/com/auction/auto_auction/service/statistic/CarStatisticService.java
+++ src/main/java/com/auction/auto_auction/service/statistic/CarStatisticService.java
@@ -1,5 +1,9 @@
 package com.auction.auto_auction.service.statistic;
 
+import com.auction.auto_auction.dto.statistic.CarStatisticDTO;
+
 
 public interface CarStatisticService {
+
+    CarStatisticDTO getTop10MostSellingMarkOfCar();
 }
