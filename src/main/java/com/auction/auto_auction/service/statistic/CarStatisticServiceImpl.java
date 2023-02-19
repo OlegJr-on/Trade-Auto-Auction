86e5d69559b8c00cafac49a6120c9fc669cf9cc1
Oleh Mandra <mandra9012003@gmail.com>
2023-02-19 21:41:04 +0200

    feat: implements interface which will provide business exercises; override methods of interface

--- src/main/java/com/auction/auto_auction/service/statistic/CarStatisticServiceImpl.java
+++ src/main/java/com/auction/auto_auction/service/statistic/CarStatisticServiceImpl.java
@@ -1,5 +1,6 @@
 package com.auction.auto_auction.service.statistic;
 
+import com.auction.auto_auction.dto.statistic.CarStatisticDTO;
 import com.auction.auto_auction.repository.uow.UnitOfWork;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -8,6 +9,21 @@ import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
-public class CarStatisticServiceImpl {
+public class CarStatisticServiceImpl implements CarStatisticService{
     private UnitOfWork unitOfWork;
+
+    @Override
+    public CarStatisticDTO getBestSellingCars() {
+        return null;
+    }
+
+    @Override
+    public CarStatisticDTO getTop10MostSellingMarkOfCar() {
+        return null;
+    }
+
+    @Override
+    public CarStatisticDTO getTop10MarkBringMostIncome() {
+        return null;
+    }
 }
