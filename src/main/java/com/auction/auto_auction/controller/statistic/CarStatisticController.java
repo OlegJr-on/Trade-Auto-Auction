d583b3aa0826fd733f8a341a89d06240751fa630
Oleh Mandra <mandra9012003@gmail.com>
2023-02-24 20:47:35 +0200

    feat: method which return top 10 car`s brands that bring most income

--- src/main/java/com/auction/auto_auction/controller/statistic/CarStatisticController.java
+++ src/main/java/com/auction/auto_auction/controller/statistic/CarStatisticController.java
@@ -1,13 +1,27 @@
 package com.auction.auto_auction.controller.statistic;
 
+import com.auction.auto_auction.dto.statistic.CarStatisticDTO;
 import com.auction.auto_auction.service.statistic.CarStatisticService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.math.BigDecimal;
+import java.util.Map;
+
 @RestController
-@RequestMapping("api-auction/statistic/cars")
+@RequestMapping("/api-auction/statistic/cars")
 @AllArgsConstructor
 public class CarStatisticController {
     private final CarStatisticService carStatisticService;
+
+    @GetMapping("/most-income")
+    public ResponseEntity<Map<String, BigDecimal>> getTop10CarBrandsBringMostIncome(){
+
+        CarStatisticDTO brandToIncome = this.carStatisticService.getTop10MarkBringMostIncome();
+
+        return ResponseEntity.ok(brandToIncome.getMostProfitableCarMarks());
+    }
 }
