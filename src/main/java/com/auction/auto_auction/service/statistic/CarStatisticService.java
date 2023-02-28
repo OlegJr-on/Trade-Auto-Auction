53f39a4a37ccacf04517c532cee9be2ae451c4d5
Oleh Mandra <mandra9012003@gmail.com>
2023-02-28 16:22:28 +0200

    feat: method which return mark cars ordered by bid activity for last 24 hours

--- src/main/java/com/auction/auto_auction/service/statistic/CarStatisticService.java
+++ src/main/java/com/auction/auto_auction/service/statistic/CarStatisticService.java
@@ -14,4 +14,6 @@ public interface CarStatisticService {
     CarStatisticDTO getTop10MostPopularMarksOfCarByBidActivity();
 
     CarStatisticDTO getTop10CarsMarkByCommissionIncome();
+
+    CarStatisticDTO getCarMarkBidActivityLast24Hours();
 }
