e23f64b0e9b86a50ba3e3933c0875ccb05447981
Oleh Mandra <mandra9012003@gmail.com>
2023-03-03 22:32:23 +0200

    feat: enable to use mongoDb repository

--- src/main/java/com/auction/auto_auction/TradeAutoAuctionApplication.java
+++ src/main/java/com/auction/auto_auction/TradeAutoAuctionApplication.java
@@ -4,10 +4,12 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableMongoRepositories
 public class TradeAutoAuctionApplication {
 
 	@Bean
