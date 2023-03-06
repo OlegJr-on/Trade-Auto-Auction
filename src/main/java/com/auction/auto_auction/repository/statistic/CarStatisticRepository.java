302d1d730f107a0144d8150642f426eb58350c27
Oleh Mandra <mandra9012003@gmail.com>
2023-03-06 13:44:43 +0200

    feat: methods which receive info about cars stats and give in time duration

--- src/main/java/com/auction/auto_auction/repository/statistic/CarStatisticRepository.java
+++ src/main/java/com/auction/auto_auction/repository/statistic/CarStatisticRepository.java
@@ -3,5 +3,15 @@ package com.auction.auto_auction.repository.statistic;
 import com.auction.auto_auction.entity.statistic.CarStatistic;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
 public interface CarStatisticRepository extends MongoRepository<CarStatistic,String> {
+
+    Optional<List<CarStatistic>> findByDateAfter(LocalDateTime event);
+
+    Optional<List<CarStatistic>> findByDateBefore(LocalDateTime event);
+
+    Optional<List<CarStatistic>> findByDateBetween(LocalDateTime start, LocalDateTime end);
 }
