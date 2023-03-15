e1245d04a42223e9eee3e2f4e7e1d89c506460cc
Oleh Mandra <mandra9012003@gmail.com>
2023-03-15 21:27:19 +0200

    fix: delete useless annotation

--- src/main/java/com/auction/auto_auction/TradeAutoAuctionApplication.java
+++ src/main/java/com/auction/auto_auction/TradeAutoAuctionApplication.java
@@ -5,10 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
 @EnableMongoRepositories
 public class TradeAutoAuctionApplication {
 
